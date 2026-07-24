b668dd258446d2405004ddf76199191bf394a360
JunYoung Choi <juny0955@gmail.com>
2026-07-24 16:04:23 +0900

    출력 형식 개선

--- crates/ip-lab/src/main.rs
+++ crates/ip-lab/src/main.rs
@@ -14,11 +14,15 @@ fn main() {
 
     match Cidr::parse(input.trim()) {
         Ok(cidr) => {
-            println!("cidr: {:?}", cidr);
-            println!("subnet_mask: {:?}", cidr.subnet_mask());
-            println!("network_address: {:?}", cidr.network_address());
-            println!("brodcast_address: {:?}", cidr.broadcast_address());
+            println!("input: {}/{}", formatting(cidr.ip), cidr.prefix_len);
+            println!("subnet_mask: {}", formatting(cidr.subnet_mask()));
+            println!("network_address: {}", formatting(cidr.network_address()));
+            println!("brodcast_address: {}", formatting(cidr.broadcast_address()));
         }
         Err(e) => eprintln!("{:?}", e),
     }
+}
+
+fn formatting(arr: [u8; 4]) -> String {
+    format!("{}.{}.{}.{}", arr[0], arr[1], arr[2], arr[3])
 }
