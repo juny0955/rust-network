d381e6eac0a9ba2b4d673c1df2bc1827e4343388
JunYoung Choi <juny0955@gmail.com>
2026-07-24 16:53:46 +0900

    be -> u16 변환

--- crates/byte-lab/src/endian.rs
+++ crates/byte-lab/src/endian.rs
@@ -1,17 +1,27 @@
-pub fn to_big_endian_bytes(value: u16) -> [u8; 2] {
+pub fn to_be_u16(value: u16) -> [u8; 2] {
     [(value >> 8) as u8, (value & 0x00FF) as u8]
+}
+
+pub fn from_be_u16(value: [u8; 2]) -> u16 {
+    (value[0] as u16) << 8 | value[1] as u16
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::endian::to_big_endian_bytes;
+    use crate::endian::{from_be_u16, to_be_u16};
 
     #[test]
-    fn bytes_big_endian_변환() {
+    fn u16_be_변환() {
         let bytes = 0x1234;
-        let be = to_big_endian_bytes(bytes);
+        let be = to_be_u16(bytes);
 
         assert_eq!(be[0], 0x12);
         assert_eq!(be[1], 0x34);
+    }
+
+    #[test]
+    fn be_u16_변환() {
+        let bytes = from_be_u16([0x12, 0x34]);
+        assert_eq!(bytes, 0x1234);
     }
 }
--- crates/byte-lab/src/main.rs
+++ crates/byte-lab/src/main.rs
@@ -1,7 +1,13 @@
-use crate::endian::to_big_endian_bytes;
+use crate::endian::{from_be_u16, to_be_u16};
 
 mod endian;
 
 fn main() {
-    println!("{:?}", to_big_endian_bytes(0x1234));
+    let bytes = 0x1234;
+
+    let to_be = to_be_u16(bytes);
+    println!("to_be: {:?}", to_be);
+
+    let from_be = from_be_u16(to_be);
+    println!("from_be: {from_be}");
 }
