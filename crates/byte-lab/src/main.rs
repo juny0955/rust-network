3f332ea164dfaf7d944c16236c06d6b1a24e280c
JunYoung Choi <juny0955@gmail.com>
2026-07-26 10:27:39 +0900

    u32 <-> be 변환

--- crates/byte-lab/src/main.rs
+++ crates/byte-lab/src/main.rs
@@ -1,13 +1,20 @@
-use crate::endian::{from_be_u16, to_be_u16};
+use crate::endian::{from_be_u16, from_be_u32, to_be_u16, to_be_u32};
 
 mod endian;
 
 fn main() {
-    let bytes = 0x1234;
+    let bytes_u16 = 0x1234;
+    let bytes_u32 = 0x12345678;
 
-    let to_be = to_be_u16(bytes);
-    println!("to_be: {:?}", to_be);
+    let to_be_u16 = to_be_u16(bytes_u16);
+    println!("to_be_u16: {:?}", to_be_u16);
 
-    let from_be = from_be_u16(to_be);
-    println!("from_be: {from_be}");
+    let from_be_u16 = from_be_u16(to_be_u16);
+    println!("from_be_u16: 0x{from_be_u16:04X}");
+
+    let to_be_u32 = to_be_u32(bytes_u32);
+    println!("to_be_u16: {:?}", to_be_u32);
+
+    let from_be_u32 = from_be_u32(to_be_u32);
+    println!("from_be_u16: 0x{from_be_u32:08X}");
 }
