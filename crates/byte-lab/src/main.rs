3652ecd5b12609ae73552e0df356eed7abeac2c5
JunYoung Choi <juny0955@gmail.com>
2026-07-26 11:10:58 +0900

    internet checksum 계산

--- crates/byte-lab/src/main.rs
+++ crates/byte-lab/src/main.rs
@@ -1,10 +1,15 @@
-use crate::endian::{from_be_u16, from_be_u32, to_be_u16, to_be_u32};
+use crate::{
+    checksum::network_checksum,
+    endian::{from_be_u16, from_be_u32, to_be_u16, to_be_u32},
+};
 
+mod checksum;
 mod endian;
 
 fn main() {
     let bytes_u16 = 0x1234;
     let bytes_u32 = 0x12345678;
+    let network_bytes = [0x00, 0x01, 0xF2, 0x03, 0xF4, 0xF5, 0xF6, 0xF7];
 
     let to_be_u16 = to_be_u16(bytes_u16);
     println!("to_be_u16: {:?}", to_be_u16);
@@ -17,4 +22,7 @@ fn main() {
 
     let from_be_u32 = from_be_u32(to_be_u32);
     println!("from_be_u32: 0x{from_be_u32:08X}");
+
+    let checksum = network_checksum(&network_bytes);
+    println!("{network_bytes:?} checksum: 0x{checksum:04X}");
 }
