1c25b6af8ef2edc02fa733d7affa0c280cbf28ed
JunYoung Choi <juny0955@gmail.com>
2026-07-27 12:17:02 +0900

    client read timeout 추가

--- crates/udp-echo/src/bin/client.rs
+++ crates/udp-echo/src/bin/client.rs
@@ -1,7 +1,10 @@
-use std::{io::Result, net::UdpSocket, str::from_utf8};
+use std::{io::Result, net::UdpSocket, str::from_utf8, time::Duration};
+
+const READ_TIMEOUT: Duration = Duration::from_secs(3);
 
 fn main() -> Result<()> {
     let socket = UdpSocket::bind("127.0.0.1:0")?;
+    socket.set_read_timeout(Some(READ_TIMEOUT))?;
 
     let mut buf = [0; 1024];
     socket.send_to(b"hello", "127.0.0.1:7000")?;
