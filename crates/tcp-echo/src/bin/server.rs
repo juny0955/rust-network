02d4282ee370fb77811ab3420e40643a36f42862
JunYoung Choi <juny0955@gmail.com>
2026-07-28 14:26:05 +0900

    tcp echo server 구현

---
+++ crates/tcp-echo/src/bin/server.rs
@@ -0,0 +1,26 @@
+use std::{
+    io::{Read, Result, Write},
+    net::TcpListener,
+};
+
+fn main() -> Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:7000")?;
+    println!("TCP server listening");
+
+    match listener.accept() {
+        Ok((mut stream, addr)) => {
+            let mut buf = [0; 1024];
+            let amt = stream.read(&mut buf)?;
+
+            if amt == 0 {
+                println!("{addr} 연결 종료");
+                return Ok(());
+            }
+
+            stream.write_all(&buf[..amt])?;
+        }
+        Err(e) => eprintln!("클라이언트 연결 오류: {e}"),
+    }
+
+    Ok(())
+}
