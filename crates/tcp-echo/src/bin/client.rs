e35b05d90eac37649cc5ea7917ca49a83c461b54
JunYoung Choi <juny0955@gmail.com>
2026-07-28 16:10:53 +0900

    client 연결 에러 처리

--- crates/tcp-echo/src/bin/client.rs
+++ crates/tcp-echo/src/bin/client.rs
@@ -1,5 +1,5 @@
 use std::{
-    io::{Read, Result, Write},
+    io::{ErrorKind, Read, Result, Write},
     net::{SocketAddr, TcpStream},
     str::from_utf8,
     time::Duration,
@@ -9,7 +9,19 @@ const CONNECT_TIMEOUT: Duration = Duration::from_secs(3);
 
 fn main() -> Result<()> {
     let addr = SocketAddr::from(([127, 0, 0, 1], 7000));
-    let mut stream = TcpStream::connect_timeout(&addr, CONNECT_TIMEOUT)?;
+    let mut stream = match TcpStream::connect_timeout(&addr, CONNECT_TIMEOUT) {
+        Ok(s) => s,
+        Err(e) => {
+            match e.kind() {
+                ErrorKind::TimedOut => eprintln!("연결 Timeout {}초", CONNECT_TIMEOUT.as_secs()),
+                ErrorKind::ConnectionRefused => eprintln!("서버 연결 실패"),
+                _ => eprintln!("연결 오류: {e}"),
+            }
+
+            return Err(e);
+        }
+    };
+
     stream.write_all(b"hello")?;
 
     let mut buf = [0; 1024];
