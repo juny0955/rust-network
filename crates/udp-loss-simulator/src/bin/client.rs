6ebc14d06f213a50da0d443f6c567b744480ec8d
JunYoung Choi <juny0955@gmail.com>
2026-08-07 09:17:52 +0900

    client 구현

---
+++ crates/udp-loss-simulator/src/bin/client.rs
@@ -0,0 +1,12 @@
+use std::{io::Result, net::{SocketAddr, UdpSocket}};
+
+fn main() -> Result<()> {
+    let socket = UdpSocket::bind("0.0.0.0:0")?;
+    let server_addr = SocketAddr::from(([127, 0, 0, 1], 9000));
+
+    for i in 0..10 {
+        socket.send_to(format!("{}:hello", {i+1}).as_bytes(), server_addr)?;
+    }
+
+    Ok(())
+}
