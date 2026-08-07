072fe682b00c992440ec2c64907e8f986209e434
JunYoung Choi <juny0955@gmail.com>
2026-08-07 14:20:35 +0900

    client 재전송

--- crates/reliable-udp/src/bin/client.rs
+++ crates/reliable-udp/src/bin/client.rs
@@ -7,22 +7,38 @@ async fn main() -> Result<()> {
     let socket = UdpSocket::bind("0.0.0.0:0").await?;
     let server_addr = SocketAddr::from(([127, 0, 0, 1], 9000));
 
-    socket.send_to("DATA:1:hello".as_bytes(), server_addr).await?;
+    let seq = 1;
+    socket
+        .send_to(format!("DATA:{seq}:hello").as_bytes(), server_addr)
+        .await?;
 
     let mut buf = [0; 1024];
-    timeout(Duration::from_secs(1), async {
-        if let Ok((amt, _)) = socket.recv_from(&mut buf).await {
-            let msg = String::from_utf8_lossy(&buf[..amt]);
-            let parts: Vec<&str> = msg.split(':').collect();
+    for _ in 0..3 {
+        match timeout(Duration::from_secs(1), socket.recv_from(&mut buf)).await {
+            Ok(Ok((amt, _))) => {
+                let msg = String::from_utf8_lossy(&buf[..amt]);
+                let parts: Vec<&str> = msg.split(':').collect();
 
-            if matches!(parts[0], "ACK") {
-                println!("메세지 전송 성공");
-            } else {
-                eprintln!("메세지 전송 오류: {}", parts[1]);
+                let resp_seq = match parts[1].parse::<u64>() {
+                    Ok(s) => s,
+                    Err(e) => {
+                        eprintln!("잘못되 응답");
+                        break;
+                    }
+                };
+
+                if matches!(parts[0], "ACK") && resp_seq == seq {
+                    println!("메세지 전송 성공");
+                } else {
+                    eprintln!("메세지 전송 오류: {}", parts[1]);
+                }
+
+                break;
             }
+            Ok(Err(e)) => return Err(e),
+            Err(_) => println!("timeout 재전송 시도"),
         }
-    })
-    .await?;
+    }
 
     Ok(())
 }
