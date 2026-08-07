183fde9b45288f2bafc26402ab37018e4d9c4e92
JunYoung Choi <juny0955@gmail.com>
2026-08-07 09:34:09 +0900

    패킷 유실 추가

--- crates/udp-loss-simulator/src/bin/client.rs
+++ crates/udp-loss-simulator/src/bin/client.rs
@@ -7,8 +7,14 @@ fn main() -> Result<()> {
     let socket = UdpSocket::bind("0.0.0.0:0")?;
     let server_addr = SocketAddr::from(([127, 0, 0, 1], 9000));
 
-    for i in 0..10 {
-        socket.send_to(format!("{}:hello", { i + 1 }).as_bytes(), server_addr)?;
+    for i in 1..11 {
+        if i % 5 == 0 {
+            println!("dropped: {i}");
+            continue;
+        }
+
+        socket.send_to(format!("{}:hello", { i }).as_bytes(), server_addr)?;
+        println!("send: {i}");
     }
     socket.send_to("END".as_bytes(), server_addr)?;
 
--- crates/udp-loss-simulator/src/bin/server.rs
+++ crates/udp-loss-simulator/src/bin/server.rs
@@ -4,16 +4,16 @@ fn main() -> Result<()> {
     let socket = UdpSocket::bind("127.0.0.1:9000")?;
 
     let mut buf = [0; 1024];
-    
+
     loop {
         if let Ok((amt, _)) = socket.recv_from(&mut buf) {
-            let msg = String::from_utf8_lossy(&buf[..amt]);  
+            let msg = String::from_utf8_lossy(&buf[..amt]);
 
             if msg == "END" {
                 break;
             }
 
-            println!("{msg:?}");
+            println!("recived: {msg:?}");
         }
     }
 
